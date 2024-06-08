0682e3f923ab34c04eb31aef8c51454edeef600c
Tanmay Bakshi <tanmay@ibm.com>
2024-06-08 17:19:06 -0400

    address nits, run formatter and linter

--- src/mlir/ir/attribute/dense_bool.rs
+++ src/mlir/ir/attribute/dense_bool.rs
@@ -1,17 +1,14 @@
 use super::impl_attribute_variant;
-use crate::{
-    support::binding::impl_unowned_mlir_value,
-    ContextRef, UnownedMlirValue,
-};
+use crate::{support::binding::impl_unowned_mlir_value, ContextRef, UnownedMlirValue};
 
 use std::{marker::PhantomData, os::raw::c_int};
 
 use mlir_sys::{
-    mlirAttributeIsADenseBoolArray, mlirDenseArrayGetNumElements,
-    mlirDenseBoolArrayGetElement, mlirDenseBoolArrayGet, MlirAttribute
+    mlirAttributeIsADenseBoolArray, mlirDenseArrayGetNumElements, mlirDenseBoolArrayGet,
+    mlirDenseBoolArrayGetElement, MlirAttribute,
 };
 
-/// [DenseBoolAttributeRef] is a reference to an instance of the `mlir::DenseBoolArrayAttr`, which
+/// [`DenseBoolAttributeRef`] is a reference to an instance of the `mlir::DenseBoolArrayAttr`, which
 /// represents a constant array of booleans in the MLIR IR.
 ///
 /// The following bindings into the MLIR C API are used/supported:
@@ -32,14 +29,14 @@ impl_unowned_mlir_value!(no_refs, DenseBoolAttributeRef, MlirAttribute);
 impl_attribute_variant!(DenseBoolAttributeRef, mlirAttributeIsADenseBoolArray);
 
 impl DenseBoolAttributeRef {
-    /// Constructs a new dense array of boolean attributes with the provided values.
+    /// Constructs a new dense array of booleans attribute with the provided values.
     ///
     /// # Arguments
     /// * `context` - The context that should own the attribute.
-    /// * `values` - The integers to hold in the attribute.
+    /// * `values` - The booleans to hold in the attribute.
     ///
     /// # Returns
-    /// Returns a reference to a new [DenseBoolAttributeRef] instance.
+    /// Returns a reference to a new [`DenseBoolAttributeRef`] instance.
     pub fn new<'a>(context: &'a ContextRef, values: &[bool]) -> &'a Self {
         let int_values: Vec<c_int> = values.iter().map(|&b| if b { 1 } else { 0 }).collect();
         unsafe {
@@ -72,7 +69,7 @@ impl DenseBoolAttributeRef {
     /// Returns the boolean at the provided index.
     pub fn get(&self, index: isize) -> bool {
         assert!(index < self.len());
-        unsafe {  mlirDenseBoolArrayGetElement(self.to_raw(), index) }
+        unsafe { mlirDenseBoolArrayGetElement(self.to_raw(), index) }
     }
 }
 
@@ -84,13 +81,11 @@ mod tests {
     #[test]
     fn test_dense_bool_attribute() {
         let context = Context::new(None, false);
-        {
-            let values = [true, false, true];
-            let attr = DenseBoolAttributeRef::new(&context, &values);
-            assert_eq!(attr.len(), 3);
-            assert_eq!(attr.get(0), true);
-            assert_eq!(attr.get(1), false);
-            assert_eq!(attr.get(2), true);
-        };
+        let values = [true, false, true];
+        let attr = DenseBoolAttributeRef::new(&context, &values);
+        assert_eq!(attr.len(), 3);
+        assert!(attr.get(0));
+        assert!(!attr.get(1));
+        assert!(attr.get(2));
     }
 }
