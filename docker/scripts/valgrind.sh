00c33fde7fc4005c7f2e8c476cd540cc86377fa8
Moses Cordeiro <moses.cordeiro@ibm.com>
2024-06-27 14:41:48 -0400

    Testing

--- docker/scripts/valgrind.sh
+++ docker/scripts/valgrind.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-output=$(cargo test 2>&1)
-echo "output: $output"
-test_binary_path=$(echo "$output" | grep 'Running unittests' | awk '{print $4}')
-echo "test_binary_path: $test_binary_path"
-test_binary_path="${$test_binary_path//[()]/}"
-echo "test_binary_path: $test_binary_path"
-echo "Running valgrind on cargo test binary: $test_binary_path"
-valgrind --leak-check=full --error-exitcode=1 --track-origins=yes --log-file="./valgrind.log" --show-leak-kinds=all "$test_binary_path"
-cat ./valgrind.log
+output=$(cargo test 2>&1) &&
+echo "output: $output" &&
+test_binary_path=$(echo "$output" | grep 'Running unittests' | awk '{print $4}') &&
+echo "test_binary_path: $test_binary_path" &&
+test_binary_path="${test_binary_path//[()]/}" &&
+echo "test_binary_path: $test_binary_path" &&
+echo "Running valgrind on cargo test binary: $test_binary_path" &&
+valgrind --leak-check=full --error-exitcode=1 --track-origins=yes --log-file="./valgrind.log" --show-leak-kinds=all "$test_binary_path" &&
+cat ./valgrind.log;
